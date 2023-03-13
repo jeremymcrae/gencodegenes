274eb7db48d54ab3d24f244029e9c034601b725b
Jeremy McRae <jmcrae@illumina.com>
2023-03-13 15:48:13 -0700

    include another high level import

--- src/gencodegenes/__init__.py
+++ src/gencodegenes/__init__.py
@@ -2,5 +2,5 @@ from pkg_resources import get_distribution
 
 __version__ = get_distribution('gencodegenes').version
 
-from gencodegenes.gencode import Gencode
+from gencodegenes.gencode import Gencode, Gene
 from gencodegenes.transcript import Transcript
