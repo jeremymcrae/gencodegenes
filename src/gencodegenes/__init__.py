92d07aac30148478dcc8ac1b6abd408fa791e411
Jeremy McRae <jmcrae@illumina.com>
2023-03-13 15:40:33 -0700

    use correct package name

--- src/gencodegenes/__init__.py
+++ src/gencodegenes/__init__.py
@@ -1,6 +1,6 @@
 from pkg_resources import get_distribution
 
-__version__ = get_distribution('bgen').version
+__version__ = get_distribution('gencodegenes').version
 
 from gencodegenes.gencode import Gencode
 from gencodegenes.transcript import Transcript
