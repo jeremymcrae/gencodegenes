16fecd1beb12621a14bc9f978a9c3a3fbe6cd671
Jeremy McRae <jmcrae@illumina.com>
2023-11-06 16:08:45 -0800

    removed pkg_resources import

--- src/gencodegenes/__init__.py
+++ src/gencodegenes/__init__.py
@@ -1,6 +1,7 @@
-from pkg_resources import get_distribution
+from importlib.metadata import version
 
-__version__ = get_distribution('gencodegenes').version
+__name__ = 'gencodegenes'
+__version__ = version('gencodegenes')
 
 from gencodegenes.gencode import Gencode, Gene
 from gencodegenes.transcript import Transcript
